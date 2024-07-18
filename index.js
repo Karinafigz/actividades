d05be53ad6e78598d79d2fa1e4449c1fe20afa4a
karinafg <gfigueroa6@ucol.mx>
2024-07-18 17:29:08 -0600

    nuevos cambios

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
-function arreglonumero(){
-    //array
-    const numbers=[11,2,31,4,5,36,7,48,95,104,131,12];
+const numeros=[11,2,31,4,5,36,7,48,95,194,131,192];
+
+function arreglonumero(numbers){
     //ordenar los numeros de menor  a mayor
-    numbers.sort((a,b)=> a-b);
-    console.log("Los numeros ordenados de menor a mayor son : " + numbers);
+    const resultado= {
 
-    //sacar el promedio de la suma del array
-    let sum = numbers.reduce((a, b) => a + b);
-    let promedio= sum / numbers.length;
-    console.log("El promedio total es: " , promedio);
+      ordenar:function(){
+        numbers.sort((a,b)=> a-b);
+        return numbers;
+      },
+      promedio:function(){        
+        //sacar el promedio de la suma del array
+        let sum = numbers.reduce((a, b) => a + b);
+        let promedio= sum / numbers.length;
+        return promedio;
+      },
+     // Método para filtrar los números primos
+     primos: function() {
+      function esPrimo(num) {
+        if (num <= 1) return false;
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+          if (num % i === 0) return false;
+        }
+        return true;
+      }
 
-  //funcion para sacar numeros primos
-  function primos(num) {
-     if (num <= 1) return false; 
-     for (let i = 2; i <= Math.sqrt(num); i++) {
-      if (num % i === 0) return false; 
+      const numerosPrimos = numeros.filter(esPrimo);
+      return numerosPrimos;
     }
-        return true; 
-    }
+  };
+// console1 ordenar
+const numerosOrdenados = resultado.ordenar();
+console.log("Los números ordenados de menor a mayor son:", numerosOrdenados);
 
-  let numeroprimos = numbers.filter(primos);
-  console.log("Los numeros primos son:", numeroprimos);
+// Console2 promedio
+const promedio = resultado.promedio();
+console.log("El promedio total es:", promedio);
 
+//console3 primos
+const numerosPrimos = resultado.primos();
+console.log("Los números primos son:", numerosPrimos);
 }
-arreglonumero();
+
+arreglonumero(numeros);
